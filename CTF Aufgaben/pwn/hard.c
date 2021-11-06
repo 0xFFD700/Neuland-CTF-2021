4d6eaa7a073cabb3403e0aae4befcec308d2699c
Jakob Löw <jakob@m4gnus.de>
2021-11-06 23:53:40 +0100

    :sparkles: add first version of pwn tasks (hard one is untested)

---
+++ CTF Aufgaben/pwn/hard.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+// for security reasons we moved the flag into flag.txt 
+// #define FLAG "nflag{dummy-flag-for-testing}"
+
+const char *courses[8] = {
+    "Maths 1",
+    "Maths 2",
+    "Object Oriented Programming",
+    "Computer Vision",
+    "IT-Security",
+    "Forensics",
+    "Software Engineering",
+    "Theoretical Computer Science",
+};
+
+int main() {
+    printf("Courses:\n");
+    for(int i = 0; i < 8; i++) {
+        printf("  - %s\n", courses[i]);
+    }
+
+    printf("\n");
+    printf("Pick a name: ");
+    fflush(stdout);
+
+    char name[32];
+    scanf("%s", name);
+
+    printf("You picked: %s, course number: ", name);
+    for(int i = 0; i < 8; i++) {
+        if(strcmp(courses[i], name) == 0) {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
